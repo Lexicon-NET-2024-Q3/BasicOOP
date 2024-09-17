04b36675033da190655fc49b53ac79627e3ffde8
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-17 13:18:06 +0200

    Added Person

---
+++ BasicOOP2024Q3/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP2024Q3
+{
+    internal class Person
+    {
+        //public string Name { get; set; } = ""; 
+        //public string? Name { get; set; } 
+        public string Name { get; set; } = string.Empty;
+
+    }
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+
+    internal class Admin : Employee
+    {
+        public string Department { get; set; } = string.Empty; 
+    }
+}
--- BasicOOP2024Q3/Program.cs
+++ BasicOOP2024Q3/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace BasicOOP2024Q3
+﻿
+namespace BasicOOP2024Q3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Person p = new Person();
+            //string name = p.Name;
+            //if(name != null)
+            //{
+            //    int lenght = name.Length;
+            //}
+
+            Person p = new Person();
+            p.Name = "Kalle";
+            Employee e = new Employee();
+            e.Name = "Nisse";
+            e.Salary = 25000;
+            Admin a = new Admin();
+            a.Name = "Lisa";
+            a.Salary = 30000;
+            a.Department = "IT";
+
+
+            Employee admin = new Admin();
+            Person admin2 = new Admin();
+            Admin admin3 = new Admin();
+
+            Do(admin);
+            Do(admin2);
+            Do(admin3);
+            Do(p);
+            Do(e);
+
+
+            List<Person> people = new List<Person>()
+            {
+               p,
+               e,
+               a,
+               admin,
+               admin2,
+               admin3
+            }; 
+        }
+
+        private static void Do(Person person)
+        {
+            Console.WriteLine(person);
         }
     }
 }
